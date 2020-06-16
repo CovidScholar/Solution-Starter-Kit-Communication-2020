aac4b1f3ecf0ed607b33cf2f6af5b35250f79557
VGG Consulting <37491845+VGGatGitHub@users.noreply.github.com>
2020-06-15 20:06:09 -0700

    updated cloud function states list

---
+++ VGG-add_ons/COVID19APIorWD_cloud_function_v2.js
@@ -0,0 +1,161 @@
+/**
+ *
+ * main() will be run when you invoke this action
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
+var request = require("request-promise");
+const DiscoveryV1 = require("watson-developer-cloud/discovery/v1");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+async function main(params) {
+
+
+  if (params.type === "api") {
+   /*
+    * Use of the google costum search
+    */
+    if (params.filter){
+         if (params.filter.toLowerCase()=="location"){
+            //VGG hard wired google api credentials 
+            var GURL='https://www.googleapis.com/customsearch/v1';
+            var Gkey='AIzaSyDMmU0d79vFkC7W8OH9aJX5g-3fiKWwn9A';
+            var Gcx='004165555931733837974:yjczek9bmlf';
+            var Gq=params.input; //'business%20reopening';
+            
+            var info = {
+                method: "GET",
+                json: true,
+                // 'customconfig=' + customConfigId,
+                //uri: "https://api.covid19api.com/summary",
+                uri: GURL+'?key='+Gkey+'&cx='+Gcx+'&q='+Gq
+                // headers: {'Ocp-Apim-Subscription-Key' : subscriptionKey}
+            }
+        };
+        
+        try {
+          const data = await request(info)
+          
+          let response = data.items.map((v, i) => {
+          return `${v.title}
+                  ${v.snippet}
+                  ${v.link}`;
+              
+          });
+          return {
+            result:
+              // "Here are the Google CSE results I found.\n\n" +
+              "\n\n" +response.join("\n\n")
+          };
+      
+        } catch (err) {
+          return { error: "it failed : " + err };
+        }
+        
+    } else {
+        /*
+         * Use of the 'Johns Hopkins CSSE' resource
+         */
+        try {
+          const summary = await request({
+            method: "GET",
+            uri: "https://api.covid19api.com/summary",
+            json: true
+          });
+    
+          if (params.country) {
+            for (var i = 0; i < summary.Countries.length; i++) {
+              if (
+                summary.Countries[i].Country.toLowerCase() ===
+                params.country.toLowerCase()
+              ) {
+                return {
+                  result: `Total Cases: ${summary.Countries[i].TotalConfirmed}\nTotal Deaths: ${summary.Countries[i].TotalDeaths}\nTotal Recovered: ${summary.Countries[i].TotalRecovered}\n\nSource: Johns Hopkins CSSE`
+                };
+              }
+            }
+            return { error: "did not find country" };
+          }
+          let totalCases = 0;
+          let totalDeaths = 0;
+          let totalRecovered = 0;
+    
+          for (var i = 0; i < summary.Countries.length; i++) {
+            totalCases += summary.Countries[i].TotalConfirmed;
+            totalDeaths += summary.Countries[i].TotalDeaths;
+            totalRecovered += summary.Countries[i].TotalRecovered;
+          }
+          return {
+            result: `Total Cases: ${totalCases}\nTotal Deaths: ${totalDeaths}\nTotal Recovered: ${totalRecovered}\n\nSource: Johns Hopkins CSSE`
+          };
+        } catch (err) {
+          return { error: "it failed : " + err };
+        }
+    }//VGG 
+    
+  } else {
+    /*
+    * Use of the 'Watson Discovery' as resource
+    */
+    const discovery = new DiscoveryV1({
+      version: "2019-03-25",
+      iam_apikey: params.api_key,
+      url: params.url
+    });
+
+    const offset = getRandomInt(50);
+
+    const queryParams = {
+      //configuration_id: params.config_id, //VGG: adding configuration_id for web crawlers 
+      environment_id: params.env_id,
+      collection_id: params.collection_id,
+      natural_language_query:
+        "corona virus " + params.input || "corona virus news",
+      filter: params.filter || '',
+      count: 3,
+      offset: offset
+    };
+    
+    /* VGG: adding filters */
+    if (params.filter){
+        if (params.filter.toLowerCase()=="funding"){
+            queryParams.filter="enriched_text.concepts.text:"+params.funding;
+            if (params.location){
+            queryParams.filter=queryParams.filter+", enriched_text.entities.text:"+params.location;
+            }
+        };
+         if (params.filter.toLowerCase()=="location"){
+            queryParams.filter="enriched_text.entities:(relevance>0.9,text::"+params.location+")";
+        };
+        
+    };
+
+    
+    try {
+      data = await discovery.query(queryParams);
+      
+      if (data.results == undefined ) {
+          return { "discovery response error" : data };
+      }
+      
+      let response = data.results.map((v, i) => {
+        return `${v.title}
+                 ${v.text}
+                 ${v.url}`;
+      });
+      return {
+        result:
+          // "Here are three news article I found online.\n\n" +
+          "\n\n" +response.join("\n\n")
+      };
+    } catch (err) {
+      return { error: "it failed : " + err };
+    }
+  }
+}
